aa203bec70e53292c7d4e6a7aed617a7249856aa
Maximiliano <maxirpc2607@gmail.com>
2020-10-02 20:33:11 -0300

    poo base
    Se comienza con el tp de poo Programacion Orientada a Objetos

---
+++ Labotatorio-2/Poo-1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Botella{
+private:
+    float capacidad;
+    float ocupacion;
+    bool tapada;
+};
+
+int main()
+{
+    cout << "Hello world! TP POO-1" << endl;
+
+
+    return 0;
+}
