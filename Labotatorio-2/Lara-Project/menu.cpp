eb6947e865983c7bf6d268000925ab6b58fbdcaf
Maximiliano <maxirpc2607@gmail.com>
2020-10-02 16:56:34 -0300

    Menu Entrenamiento
    Se crea menu entrenamiento

--- Labotatorio-2/Lara-Project/menu.cpp
+++ Labotatorio-2/Lara-Project/menu.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #include "menu.h"
 using namespace rlutil;
 
+// MENU PRINCIPAL
 void menuPrincipal()
 {
 
@@ -30,6 +31,46 @@ void menuPrincipal()
                 menuUsuarios();
             break;
             case 2:
+                menuEntrenamientos();
+            break;
+            case 3:
+                //menuReportes();
+            break;
+            case 4:
+                //menuConfiguracion();
+            break;
+            case 0:
+                return;
+            break;
+        }
+    }
+}
+
+// MENU USUARIO
+void menuUsuarios()
+{
+
+    while(true){
+        cls(); // Elimina lo que hay en la pantalla
+        //Ingresar el titulo como ejemplo, colores definidos en ui.h
+        title("MENÚ USUARIOS", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);
+        gotoxy(1, 5); //Indica la posicion despues del titulo, 1 = COLUMNA , 5 FILA
+        cout << "1) NUEVO USUARIO" << endl;
+        cout << "2) MODIFICAR USUARIO" << endl;
+        cout << "3) LISTAR USUARIO POR ID" << endl;
+        cout << "4) LISTAR TODOS LOS USUARIOS" << endl;
+        cout << "5) ELIMINAR USUARIO" << endl;
+        cout << "------------------" << endl;
+        cout << "0) VOLVER AL MENÚ PRINCIPAL" << endl;
+        int pos;
+        cout << endl << "> ";
+        cin >> pos;
+
+        switch(pos){
+            case 1:
+                //menuUsuarios();
+            break;
+            case 2:
                 //menuEntrenamientos();
             break;
             case 3:
@@ -45,7 +86,8 @@ void menuPrincipal()
     }
 }
 
-void menuUsuarios()
+// MENU ENTRENAMIENTO
+void menuEntrenamientos()
 {
 
     while(true){
@@ -53,11 +95,11 @@ void menuUsuarios()
         //Ingresar el titulo como ejemplo, colores definidos en ui.h
         title("MENÚ USUARIOS", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);
         gotoxy(1, 5); //Indica la posicion despues del titulo, 1 = COLUMNA , 5 FILA
-        cout << "1) NUEVO USUARIO" << endl;
-        cout << "2) MODIFICAR USUARIO" << endl;
-        cout << "3) LISTAR USUARIO POR ID" << endl;
-        cout << "4) LISTAR TODOS LOS USUARIOS" << endl;
-        cout << "5) ELIMINAR USUARIO" << endl;
+        cout << "1) NUEVO ENTRENAMIENTO" << endl;
+        cout << "2) MODIFICAR ENTRENAMIENTO" << endl;
+        cout << "3) LISTAR ENTRENAMIENTO POR ID" << endl;
+        cout << "4) LISTAR ENTRENAMIENTO POR IDUSUARIO" << endl;
+        cout << "5) LISTAR TODOS LOS ENTRENAMIENTO" << endl;
         cout << "------------------" << endl;
         cout << "0) VOLVER AL MENÚ PRINCIPAL" << endl;
         int pos;
