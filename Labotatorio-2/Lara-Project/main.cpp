da5fe054ed66355411c32dbf94080ac3b158097b
Maximiliano <maxirpc2607@gmail.com>
2020-10-02 14:02:30 -0300

    Base v2
    Se cargan los cpp de interfaz

--- Labotatorio-2/Lara-Project/main.cpp
+++ Labotatorio-2/Lara-Project/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <iomanip>
+#include <cstring>
 using namespace std;
-
+#include "ui.h"
+#include "rlutil.h"
 int main()
 {
+    initUI();
     cout << "Hello world!" << endl;
     return 0;
 }
