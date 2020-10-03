805fa82df40dd2c28fa8317efd1ec129dc293227
Maximiliano <maxirpc2607@gmail.com>
2020-10-03 19:04:59 -0300

    objeto Botella
    Se creo objeto botella base y su capacidad por defecto y con posibilidad de poder ingresar capacidad.

--- Labotatorio-2/Poo-1/main.cpp
+++ Labotatorio-2/Poo-1/main.cpp
@@ -14,12 +14,34 @@ public:
     void vaciar(float);
     void tapar();
     void destapar();
+    Botella(float);
 };
+
+Botella::Botella(float cap){
+    capacidad = 100;
+    ocupacion = 0;
+    if(cap != 0){
+        capacidad = cap;
+    }
+    cout << "*Se creo nueva Botella con " << capacidad << " ml." << endl;
+    cout << "-Su ocupacion es de " << ocupacion << " ml."<< endl;
+}
+
+//void llenar(float){
+
+//}
+
 
 int main()
 {
-    cout << "Hello world! TP POO-1" << endl;
+    cout << "========== Hello world! TP POO-1 ==========" << endl;
+    cout << " " << endl;
+    cout << "Ingresar Capacidad de la botella (por defecto 100 ml)" << endl;
+    float inicial;
+    cout << "> ";
+    cin >> inicial;
+    Botella bot(inicial);
 
-
+    cout << " " << endl;
     return 0;
 }
