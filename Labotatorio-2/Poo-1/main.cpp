723c0ad561ad455ee045bda468bc7d4ce0c01af1
Maximiliano <maxirpc2607@gmail.com>
2020-10-03 21:23:35 -0300

    se creo metodo
    se agrego metodo aumentar ocupacion y mostrar ocupacion actual

--- Labotatorio-2/Poo-1/main.cpp
+++ Labotatorio-2/Poo-1/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+#include <iomanip>
+#include <cstring>
 using namespace std;
+#include "ui.h"
+#include "rlutil.h"
+using namespace rlutil;
 
 class Botella{
 private:
@@ -9,12 +13,15 @@ private:
     bool tapada;
 
 public:
-    void llenar(float);
-    void vaciar();
-    void vaciar(float);
-    void tapar();
-    void destapar();
+    void setLlenar(float);
+    void setVaciar();
+    void setVaciar(float);
+    void getTapar();
+    void getDestapar();
     Botella(float);
+    float getCapacidad();
+    float getOcupacion();
+    float getDisponibilidad();
 };
 
 Botella::Botella(float cap){
@@ -27,21 +34,70 @@ Botella::Botella(float cap){
     cout << "-Su ocupacion es de " << ocupacion << " ml."<< endl;
 }
 
-//void llenar(float){
+void Botella::setLlenar(float aumentar){
+    ocupacion += aumentar;
+}
 
-//}
-
-
+float Botella::getOcupacion(){
+    return ocupacion;
+}
 int main()
 {
-    cout << "========== Hello world! TP POO-1 ==========" << endl;
-    cout << " " << endl;
+    initUI();
+    title("HELLO WORLD TP POO-1", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);
+    gotoxy(1, 5);
     cout << "Ingresar Capacidad de la botella (por defecto 100 ml)" << endl;
     float inicial;
     cout << "> ";
     cin >> inicial;
     Botella bot(inicial);
 
-    cout << " " << endl;
-    return 0;
+    while(true){
+        cls();
+        title("HELLO WORLD TP POO-1", APP_TITLEFORECOLOR, APP_TITLEBACKCOLOR);
+        gotoxy(1, 5);
+
+        cout << " " << endl;
+        cout << "1) LLENAR (DEBE SER MENOR A LA CAPACIDAD INDICADA)" << endl;
+        cout << "2) VACIAR DE A POCO" << endl;
+        cout << "3) VACIAR" << endl;
+        cout << "4) TAPAR BOTELLA" << endl;
+        cout << "5) DESTAPAR BOTELLA" << endl;
+        cout << "6) VER INFO DE BOTELLA" << endl;
+        cout << "------------------" << endl;
+        cout << "0) SALIR DEL PROGRAMA" << endl;
+        int pos;
+        cout << endl << "> ";
+        cin >> pos;
+
+        switch(pos){
+            case 1:
+                cout << "INDIQUE N° DE ml PARA AUMENTAR SU OCUPACION: "<< endl;
+                cout << endl << "> ";
+                float aumentar;
+                cin >> aumentar;
+                bot.setLlenar(aumentar);
+                cout<< "LA OCUPACIÓN ACTUAL ES: " << bot.getOcupacion() << endl;
+                anykey();
+            break;
+            case 2:
+                //menuEntrenamientos();
+            break;
+            case 3:
+                //menuReportes();
+            break;
+            case 4:
+                //menuConfiguracion();
+            break;
+            case 5:
+                //menuConfiguracion();
+            break;
+            case 6:
+                //menuConfiguracion();
+            break;
+            case 0:
+                return 0;
+            break;
+        }
+    }
 }
