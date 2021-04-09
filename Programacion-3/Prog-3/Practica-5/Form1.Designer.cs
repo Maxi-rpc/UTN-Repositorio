3e8edec3a75f6832ab7099d23e1237769b17181c
Maximiliano <maxirpc2607@gmail.com>
2021-04-08 21:51:43 -0300

    Actividad 1 completa

---
+++ Programacion-3/Prog-3/Practica-5/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace Practica_5
+{
+    partial class DatosPersonales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelApellido = new System.Windows.Forms.Label();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.textBoxEdad = new System.Windows.Forms.TextBox();
+            this.labelEdad = new System.Windows.Forms.Label();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.labelDireccion = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.textBoxResultado = new System.Windows.Forms.TextBox();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelApellido
+            // 
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelApellido.Location = new System.Drawing.Point(104, 57);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(88, 20);
+            this.labelApellido.TabIndex = 0;
+            this.labelApellido.Text = "APELLIDO";
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxApellido.Location = new System.Drawing.Point(209, 57);
+            this.textBoxApellido.MaxLength = 20;
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(344, 20);
+            this.textBoxApellido.TabIndex = 1;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxNombre.Location = new System.Drawing.Point(209, 103);
+            this.textBoxNombre.MaxLength = 20;
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(344, 20);
+            this.textBoxNombre.TabIndex = 3;
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.Location = new System.Drawing.Point(104, 103);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(79, 20);
+            this.labelNombre.TabIndex = 2;
+            this.labelNombre.Text = "NOMBRE";
+            // 
+            // textBoxEdad
+            // 
+            this.textBoxEdad.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxEdad.Location = new System.Drawing.Point(209, 152);
+            this.textBoxEdad.MaxLength = 3;
+            this.textBoxEdad.Name = "textBoxEdad";
+            this.textBoxEdad.Size = new System.Drawing.Size(70, 20);
+            this.textBoxEdad.TabIndex = 5;
+            this.textBoxEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxEdad_KeyPress);
+            // 
+            // labelEdad
+            // 
+            this.labelEdad.AutoSize = true;
+            this.labelEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEdad.Location = new System.Drawing.Point(104, 152);
+            this.labelEdad.Name = "labelEdad";
+            this.labelEdad.Size = new System.Drawing.Size(55, 20);
+            this.labelEdad.TabIndex = 4;
+            this.labelEdad.Text = "EDAD";
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxDireccion.Location = new System.Drawing.Point(209, 192);
+            this.textBoxDireccion.MaxLength = 20;
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(344, 20);
+            this.textBoxDireccion.TabIndex = 7;
+            // 
+            // labelDireccion
+            // 
+            this.labelDireccion.AutoSize = true;
+            this.labelDireccion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDireccion.Location = new System.Drawing.Point(104, 192);
+            this.labelDireccion.Name = "labelDireccion";
+            this.labelDireccion.Size = new System.Drawing.Size(99, 20);
+            this.labelDireccion.TabIndex = 6;
+            this.labelDireccion.Text = "DIRECCIÓN";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(104, 233);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(108, 20);
+            this.labelResultado.TabIndex = 8;
+            this.labelResultado.Text = "RESULTADO";
+            // 
+            // textBoxResultado
+            // 
+            this.textBoxResultado.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxResultado.Location = new System.Drawing.Point(108, 256);
+            this.textBoxResultado.MaxLength = 100;
+            this.textBoxResultado.Multiline = true;
+            this.textBoxResultado.Name = "textBoxResultado";
+            this.textBoxResultado.Size = new System.Drawing.Size(445, 115);
+            this.textBoxResultado.TabIndex = 9;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(478, 377);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 10;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(397, 377);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAceptar.TabIndex = 11;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            this.buttonAceptar.Click += new System.EventHandler(this.buttonAceptar_Click);
+            // 
+            // DatosPersonales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.textBoxResultado);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.labelDireccion);
+            this.Controls.Add(this.textBoxEdad);
+            this.Controls.Add(this.labelEdad);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.labelApellido);
+            this.Name = "DatosPersonales";
+            this.Text = "Datos Personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelApellido;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.TextBox textBoxEdad;
+        private System.Windows.Forms.Label labelEdad;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.Label labelDireccion;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.TextBox textBoxResultado;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Button buttonAceptar;
+    }
+}
+
---
+++ Programacion-3/Prog-3/Practica-5/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_5
+{
+    public partial class DatosPersonales : Form
+    {
+        public DatosPersonales()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxEdad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            int contarError = 0;
+
+            if(textBoxApellido.Text == "")
+            {
+                textBoxApellido.BackColor = Color.Red;
+                contarError++;
+            }
+
+            if (textBoxNombre.Text == "")
+            {
+                textBoxNombre.BackColor = Color.Red;
+                contarError++;
+            }
+
+            if (textBoxEdad.Text == "")
+            {
+                textBoxEdad.BackColor = Color.Red;
+                contarError++;
+            }
+
+            if (textBoxDireccion.Text == "")
+            {
+                textBoxDireccion.BackColor = Color.Red;
+                contarError++;
+            }
+
+            if(contarError == 0)
+            {
+                textBoxResultado.Text = "Apellido y Nombre: " + textBoxApellido.Text + " " + textBoxNombre.Text;
+                textBoxResultado.Text += "\r\n" + "Edad: " + textBoxEdad.Text;
+                textBoxResultado.Text += "\r\n" + "Dirección: " + textBoxDireccion.Text;
+            } else
+            {
+                textBoxResultado.Text = "COMPLETAR TODOS LOS DATOS";
+            }
+        }
+    }
+}
